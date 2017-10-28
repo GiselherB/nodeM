2665855b8410d5573312a36ce0dac85ad90504fd
GiselherB <33169530+GiselherB@users.noreply.github.com>
2017-10-28 09:28:53 +0530

    Readfile

---
+++ Readline.js
@@ -0,0 +1,33 @@
+Code  Issues 0  Pull requests 0  Pulse
+C_04_2-readline.js
+
+var readline = require('readline');
+var rl = readline.createInterface(process.stdin, process.stdout);
+//Creates an JavaScript object to store data
+var realPerson = {
+  name: '',
+  favourites: []
+};
+
+rl.question("What is Your name? ", function(personName){
+
+  rl.setPrompt(`What things ${personName} favourite to ??  `);
+  rl.prompt();
+
+  rl.on('line', function(favourites){
+
+    if(favourites.toLowerCase().trim() === 'exit'){
+      rl.close();
+    }else{
+      realPerson.favourites.push(favourites.trim());
+      rl.setPrompt(`what else ${personName} likes ? ('exit' to leave)  `);
+      rl.prompt();
+    }
+  });
+});
+
+rl.on('close', function(){
+  console.log("%s like to %j", realPerson.name, realPerson.favourites);
+  //%j is to print a Json array
+  process.exit();
+});
